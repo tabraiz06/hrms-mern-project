5dffdb0635aa8a894d65e7037f4071705bf6e160
Md Tabraiz <mohammadtabraiz3@gmail.com>
2024-12-26 13:01:21 +0530

    update in index.js file

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,10 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/candidates", candidateRoutes);
 app.use("/api/employees", employeeRoutes);
